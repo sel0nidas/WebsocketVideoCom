f629ba631e78f5fd8f01544baf5c86a04a2db6a7
Selahattin Emre Kutlu <48654612+sel0nidas@users.noreply.github.com>
2023-02-26 12:27:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const io = require("socket.io")(http, {cors: {
-    origin: "http://localhost:8080"
+    origin: "*"
   }
 				      });
 
