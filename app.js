2a57d692580ba103b3b67e4d76524f72babed6d2
Selahattin Emre Kutlu <selahattinkutlu76@gmail.com>
2024-01-28 23:18:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 
-
+/*
 const io = require("socket.io")(http, {
   cors: {
     origin: ["http://remotevideocontroller.onrender.com", "chrome-extension://hkbagmafpdcfpjoeaookbcbjbijdmdnf"],
@@ -14,9 +14,11 @@ const io = require("socket.io")(http, {
     credentials: true
   }
 });
+*/
 
 
-//const io = require("socket.io")(http, { cors: '*' });
+const io = require("socket.io")(http, { cors: '*' });
+
 /*
 const io = require("socket.io")(http, { 
   cors: {
